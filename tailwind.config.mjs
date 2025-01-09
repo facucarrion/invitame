4cfb095670e0487c13fc6b13eff8167983438acb
Facundo Carrion <facundojcarrion@gmail.com>
2025-01-09 17:47:12 -0300

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#dcaf66'
+      },
+      fontFamily: {
+        mallorga: ['Mallorga', 'sans-serif'],
+        adelia: ['Adelia', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
+}
